f6092b7d7192c87715fe9a995d5e47053112cedb
Jose Antonio González Alcántara <jagascripts@gmail.com>
2022-05-26 22:59:24 +0200

    Dtos mapeados

--- Gestor-Multimedia/src/test/java/FOIGestorMultimedia/GestorMultimediaApplicationTests.java
+++ Gestor-Multimedia/src/test/java/FOIGestorMultimedia/GestorMultimediaApplicationTests.java
@@ -1,9 +1,7 @@
 package FOIGestorMultimedia;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class GestorMultimediaApplicationTests {
 
 	@Test
