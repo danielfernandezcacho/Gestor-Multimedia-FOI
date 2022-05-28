c498f519e7d0ef10c3aa3146832f0c3664798ca9
JoseAGonzalezA <jose.a.gonzalez.alcantara@gmail.com>
2022-05-29 01:09:44 +0200

    Se ha cambiado la referencia a la id

--- src/main/java/FOIGestorMultimedia/dao/IUsuarioDAO.java
+++ src/main/java/FOIGestorMultimedia/dao/IUsuarioDAO.java
@@ -3,6 +3,6 @@ package FOIGestorMultimedia.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import FOIGestorMultimedia.dto.Usuario;
 
-public interface IUsuarioDAO extends JpaRepository<Usuario, String> {
+public interface IUsuarioDAO extends JpaRepository<Usuario, Integer> {
 
 }
