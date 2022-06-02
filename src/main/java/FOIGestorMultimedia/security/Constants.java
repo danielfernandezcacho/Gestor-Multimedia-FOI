91a17687b9d8a3bc7f6099eece2930417671b321
Jose Antonio González Alcántara <jagascripts@gmail.com>
2022-06-02 20:30:55 +0200

    testing

--- src/main/java/FOIGestorMultimedia/security/Constants.java
+++ src/main/java/FOIGestorMultimedia/security/Constants.java
@@ -9,9 +9,9 @@ public class Constants {
 	public static final String TOKEN_BEARER_PREFIX = "Bearer ";
 
 	// JWT
-
+	
 	public static final String ISSUER_INFO = "Administrador";
-	public static final String SUPER_SECRET_KEY = "4321";
+	public static final String SUPER_SECRET_KEY = "password";
 	public static final long TOKEN_EXPIRATION_TIME = 864_000_000; // 10 day
 
 }
